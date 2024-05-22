d058be709f63f25401ec1ec3c5efce4fddd582df
Pravar08 <pravarmishra08@gmail.com>
2024-05-22 19:16:46 +0530

    changespravar

--- components/sub/HeroContent.tsx
+++ components/sub/HeroContent.tsx
@@ -32,14 +32,14 @@ const HeroContent = () => {
           variants={slideInFromLeft(0.5)}
           className="flex flex-col gap-6 mt-6 text-6xl font-bold text-white max-w-[600px] w-auto h-auto"
         >
-        <span>
+      <span>
   Upwork and
   <span className="text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-cyan-500">
-    {" outwork"}
+    {' outwork'}
   </span>
+</span>
 
        
-          </span>
         </motion.div>
 
         <motion.p
