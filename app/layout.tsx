1d1423a1a74bfc7d20d03063a37ac9bfa4d6f8c1
Pravar08 <pravarmishra08@gmail.com>
2024-05-22 19:04:08 +0530

    changespRavar

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,8 +8,8 @@ import Footer from "@/components/main/Footer";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Space Portfolio",
-  description: "This is my portfolio",
+  title: "API Global",
+  description: "Techies",
 };
 
 export default function RootLayout({
--- components/sub/HeroContent.tsx
+++ components/sub/HeroContent.tsx
@@ -32,13 +32,12 @@ const HeroContent = () => {
           variants={slideInFromLeft(0.5)}
           className="flex flex-col gap-6 mt-6 text-6xl font-bold text-white max-w-[600px] w-auto h-auto"
         >
-          <span>
-          Upwork and
-            <span className="text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-cyan-500">
-              {" "}
-              outwork
-             
-            </span>
+        <span>
+  Upwork and
+  <span className="text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-cyan-500">
+    {" outwork"}
+  </span>
+
        
           </span>
         </motion.div>
